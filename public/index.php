522502797719e61b0880ce6c82cd83740bea2e42
Jake Tobak <jtobak@gmail.com>
2013-07-14 12:57:27 -0400

    Add file upload

--- public/index.php
+++ public/index.php
@@ -14,14 +14,28 @@ if(isset($_POST['contact']['submit'])) {
 	$recipient = 'info@megatongraphics.com';
 	$subject = "Megaton Contact Form - " . $_POST['contact']['name'];
 
+	$attachment = "";
+
+	if($_FILES['imageFile']['size'] < 20000) {
+		$zip = new ZipArchive();
+		$zipLocation = tempnam("/tmp", "zip"); 
+		$zip->open($zipLocation, ZipArchive::CREATE)
+		$zip->addFile($_FILES['imageFile']['tmp_name'],$_FILES['imageFile']['name']);
+		$zip->close();
+
+		$attachment = chunk_split(base64_encode(file_get_contents($zipLocation))); 
+	}
+
 	// main header
 	$header  = "From: " . $_POST['contact']['name'] . " <" . $_POST['contact']['email'] . ">" . $eol;
+	$header .= "Reply-To: " . $_POST['contact']['email'] . $eol;
 	$header .= "MIME-Version: 1.0" . $eol;
 	$header .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"";
 
 	// no more headers after this, we start the body! //
 	$body = "--" . $separator . $eol;
-	$body .= "Content-Transfer-Encoding: 7bit" . $eol;
+	$body .= "Content-Type: application/zip; name=\"" . $_POST['contact']['name'] . ".zip\"" . $eol;  
+	$body .= "Content-Transfer-Encoding: base64" . $eol;
 	$body .= $eol;
 
 	// message
@@ -116,6 +130,7 @@ if(isset($_POST['contact']['submit'])) {
 				<input type="text" name="contact[name]" placeholder="Name" /><br />
 				<input type="text" name="contact[email]" placeholder="e-mail" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}" /><br />
 				<textarea name="contact[message]" placeholder="Message"></textarea><br />
+				<input type="file" name="imageFile" accept="image/x-png, image/gif, image/jpeg" />
 			</div>
 			<div class="col2right">
 				<input type="submit" name="contact[submit]" value="submit" /><br />
