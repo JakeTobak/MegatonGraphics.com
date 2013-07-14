15a9ed6c0c24718bf18b6da16618d9a847a0c547
Jake Tobak <jtobak@gmail.com>
2013-07-14 14:20:52 -0400

    WTF

--- public/index.php
+++ public/index.php
@@ -33,12 +33,12 @@ if(isset($_POST['contact']['submit'])) {
 	$header .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"";
 
 	// no more headers after this, we start the body! //
-	// $body = "--" . $separator . $eol;
-	// $body .= "Content-Type: application/zip; name=\"" . $_POST['contact']['name'] . ".zip\"" . $eol;  
-	// $body .= "Content-Transfer-Encoding: 7bit" . $eol;
-	// $body .= "Content-Disposition: attachment" . $eol;
-	// $body .= $eol;
-	// $body .= $attachment;
+	$body = "--" . $separator . $eol;
+	$body .= "Content-Type: application/zip; name=\"" . $_POST['contact']['name'] . ".zip\"" . $eol;  
+	$body .= "Content-Transfer-Encoding: 7bit" . $eol;
+	$body .= "Content-Disposition: attachment" . $eol;
+	$body .= $eol;
+	$body .= $attachment;
 	//$body .= chunk_split(base64_encode(file_get_contents($_FILES['imageFile']['tmp_name']))) . $eol;
 
 	// message
@@ -134,7 +134,7 @@ if(isset($_POST['contact']['submit'])) {
 				<input type="text" name="contact[email]" placeholder="e-mail" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}" /><br />
 				<textarea name="contact[message]" placeholder="Message"></textarea><br />
     			<input type="hidden" name="MAX_FILE_SIZE" value="20000" />
-				<!--input type="file" name="imageFile" accept="image/x-png, image/png, application/postscript, application/zip, image/bmp, application/x-rar-compressed, application/octet-stream, image/x-windows-bmp, image/gif, image/jpeg, image/svg+xml" /-->
+				<input type="file" name="imageFile" accept="image/x-png, image/png, application/postscript, application/zip, image/bmp, application/x-rar-compressed, application/octet-stream, image/x-windows-bmp, image/gif, image/jpeg, image/svg+xml" />
 			</div>
 			<div class="col2right">
 				<input type="submit" name="contact[submit]" value="submit" /><br />
